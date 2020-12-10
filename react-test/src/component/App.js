c26fd058709414eb2cc0d3e4e1df690fa9083f10
Andrii Krychkivskyi <kruchandriy@gmail.com>
2020-12-10 16:54:27 +0200

    add form

--- react-test/src/component/App.js
+++ react-test/src/component/App.js
@@ -1,98 +1,107 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import TaskList from './TaskList'
-import TaskEditor from './TaskEditor'
-import  createTask from './create-task'
-import Filter from './Filter'
+import TaskList from './TaskList';
+import TaskEditor from './TaskEditor';
+import createTask from './create-task';
+import Filter from './Filter';
 // import PropTypes from 'prop-types';
 
 // const newTask = createTask()
 
 export default class App extends Component {
-    state = {
-        tasks: [],
-        filter: '',
+  state = {
+    tasks: [],
+    filter: '',
+  };
+
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate');
+    localStorage.setItem('tasks', JSON.stringify(this.state.tasks));
+  }
+
+  changeFilter = filter => {
+    console.log(filter);
+    this.setState({ filter });
+  };
+
+  getVisibleTasks = () => {
+    const { tasks, filter } = this.state;
+    return tasks.filter(task =>
+      task.text.toLowerCase().includes(filter.toLowerCase()),
+    );
+  };
+
+  addTask = text => {
+    // const task = createTask();
+    const task = {
+      id: uuidv4(),
+      text,
+      completed: false,
     };
-
-    changeFilter = filter => {
-        console.log(filter);
-        this.setState({filter})
-    }
-
-    getVisibleTasks = () => {
-        const {tasks, filter} = this.state;
-        return tasks.filter(task => task.text.toLowerCase().includes(filter.toLowerCase()))
-    }
-
-    addTask = text => {
-        // const task = createTask();
-        const task = {
-            id: uuidv4()  ,
-            text,
-            completed: false,
-        }
-        createTask();
-        this.setState(prevState => {
-            return {
-                tasks: [...prevState.tasks, task]
-            }
-        })
-    }
-
-    removeTask = taskId => {
-        // console.log(taskId);
-//   console.log('delete Task');
-  this.setState (prevState => {
+    // createTask();
+    this.setState(prevState => {
       return {
-          tasks: prevState.tasks.filter(task => task.id !== taskId)
-      }
-  })
-    }
+        tasks: [...prevState.tasks, task],
+      };
+    });
+  };
 
-    // updateCompleted = taskId => {
-    //     this.setState(prevState => {
-    //         return {
-    //             tasks: prevState.tasks.map(task => {
-    //                 if (task.id === taskId) {
-    //                     return {
-    //                         ...task,
-    //                         completed: !task.completed,
-    //                     }
-    //                 }
-    //                 return task;
-    //             })
-    //         }
-    //     })
-    // }
+  removeTask = taskId => {
+    // console.log(taskId);
+    //   console.log('delete Task');
+    this.setState(prevState => {
+      return {
+        tasks: prevState.tasks.filter(task => task.id !== taskId),
+      };
+    });
+  };
 
-    updateCompleted = taskId => {
-        this.setState(prevState =>({
-            tasks: prevState.tasks.map(task => 
-                task.id === taskId ? {...task, completed: !task.completed} : task,
-                )
-        }));
-    }
+  // updateCompleted = taskId => {
+  //     this.setState(prevState => {
+  //         return {
+  //             tasks: prevState.tasks.map(task => {
+  //                 if (task.id === taskId) {
+  //                     return {
+  //                         ...task,
+  //                         completed: !task.completed,
+  //                     }
+  //                 }
+  //                 return task;
+  //             })
+  //         }
+  //     })
+  // }
 
+  updateCompleted = taskId => {
+    this.setState(prevState => ({
+      tasks: prevState.tasks.map(task =>
+        task.id === taskId ? { ...task, completed: !task.completed } : task,
+      ),
+    }));
+  };
 
-render(){
- const {filter} = this.state;
+  render() {
+    const { filter, tasks } = this.state;
 
- const visibleTasks = this.getVisibleTasks()
-    return (  
-       <>
-        <TaskEditor onAddTask = {this.addTask}/>
-        {visibleTasks.length > 1 &&
-        <Filter value = {filter}
-         onChangeFilter = {this.changeFilter}/>
-        }
-        {visibleTasks.length >0 && (
-        <TaskList 
-        tasks = {visibleTasks} 
-        onRemoveTask ={this.removeTask}
-        onUpdateTask={this.updateCompleted}
-        /> )}
-        
-       </>
-    )
-} 
+    const visibleTasks = this.getVisibleTasks();
+    return (
+      <>
+        <TaskEditor onAddTask={this.addTask} />
+        {tasks.length > 1 && (
+          <Filter value={filter} onChangeFilter={this.changeFilter} />
+        )}
+        {visibleTasks.length > 0 && (
+          <TaskList
+            tasks={visibleTasks}
+            onRemoveTask={this.removeTask}
+            onUpdateTask={this.updateCompleted}
+          />
+        )}
+      </>
+    );
+  }
 }
--- react-test/src/component/TaskEditor.js
+++ react-test/src/component/TaskEditor.js
@@ -1,41 +1,44 @@
 // import { Component } from "react"
-import React, {Component} from 'react'
+import React, { Component } from 'react';
 
 export default class Taskeditor extends Component {
-    state = {
-      text: '',
-    }
-    handleChange = e => {
-      this.setState({
-        text: e.target.value
-      })
-    }
+  state = {
+    text: '',
+  };
+  handleChange = e => {
+    this.setState({
+      text: e.target.value,
+    });
+  };
 
-    handleSubmit = e => {
-      e.preventDefault();
-      console.log("Submit");
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log('Submit');
+    if (!this.state.text) return;
 
-      this.props.onAddTask(this.state.text);
-      
-      this.setState({text: ''})
-    }
+    this.props.onAddTask(this.state.text);
 
-
+    this.setState({ text: '' });
+  };
 
   render() {
-  return (
-     <form onSubmit = {this.handleSubmit}>
-       <label>
-       <input type= "text"
-       value = {this.state.text}
-       onChange = {this.handleChange}/>
-       </label>
-       <button type = "submit">Create notification</button>
-     </form>
-    )
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          <input
+            type="text"
+            value={this.state.text}
+            onChange={this.handleChange}
+          />
+        </label>
+        <button type="submit">Create notification</button>
+      </form>
+    );
   }
 }
 
-{/* <div>
+{
+  /* <div>
 <button type="button" onClick = {onAddTask}>Add Task</button>      
-  </div> */}
+  </div> */
+}
